63443e6dd892815157e5dcbe16c081df13d85aef
GeorgeDV <georgedevries909@hotmail.com>
2022-11-03 19:13:42 +1100

    got class UI working, forgot to pass book in UI.addBookToList()

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
+
 /*
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = () => {
-        return(title + " by " + author + ", " + pages + ", " + read)
-    }
-}
-
-const theHobbit = new Book("the hobbit", "J.R.R Tolkien", "295 pages", "not read yet")
-
-console.log(theHobbit.info())
-*/
-
 let myLibary = [];
 
 function Book(title, author, pages, read) {
@@ -30,16 +16,29 @@ function Book(title, author, pages, read) {
 Book.prototype.addBookToLibary = function() {
     myLibary.push(title, author, pages, read)
 }
-*/
 
 
-const theHobbit = new Book("the hobbit", "J.R.R Tolkien", "295 pages", "not read yet")
-const shortStory = new Book("Short Story", "Peter Carey", "127 pages", "read")
-const heartOfDarkness = new Book("Heart Of Darkness", "J Conrad", "read")
+const theHobbit = new Book("the hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+const shortStory = new Book("Short Story", "Peter Carey", "127 pages", "read");
+const heartOfDarkness = new Book("Heart Of Darkness", "J Conrad", "read");
 
 function addBookToLibary(theBook, theBook1, theBook2) {
-    myLibary.push(theBook, theBook1, theBook2)
-    console.log(myLibary)
+    myLibary.push(theBook, theBook1, theBook2);
 }
 
-addBookToLibary(shortStory, theHobbit, heartOfDarkness)
+const table = document.querySelector(".container");
+const row = document.createElement("tr");
+const cell = document.createElement("td");
+
+for (let i = 0; i < myLibary.length; i++) {
+    row.appendChild(cell);
+    cell.appendChild(myLibary[i]);
+}
+
+
+
+
+
+addBookToLibary(shortStory, theHobbit, heartOfDarkness);
+document.getElementById("stuff").innerHTML = myLibary;
+
--- script2.js
+++ script2.js
@@ -14,21 +14,38 @@ class Book {
 
 class UI {
     static displayBooks() {
-        let myLibary = [
+        const MyLibary = [
             {title: "The Hobbit", author: "JRR Tolkien", pages: "295 pages", read: "not read yet"},
             {title: "Short Story", author: "Peter Carey", pages: "127 pages", read: "read"},
             {title: "Heart Of Darkness", author: "J Conrad", pages: "89 pages", read: "read"}
         ];
 
-        const books = myLibary;
+        const books = MyLibary;
 
-        books.forEach((book) => UI.addBookToList())
+        books.forEach((book) => UI.addBookToList(book))
     }
 
     static addBookToList(book) {
+        const list = document.getElementById("book-list");
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.pages}</td>
+            <td>${book.title}</td>
+            <td><button><a href="#">X</a></button></td>
+            
+            
+        `;
+        list.appendChild(row);
 
     }
 }
+
+// Display Books (Event)
+
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 
 
